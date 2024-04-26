c5b64d2ef7917b60e13b26e6a01a357e98ba6a39
angelcruz07 <angelcruzsanchez126@gmail.com>
2024-04-26 15:13:16 -0600

    Simple router in php

---
+++ controllers/PageController.php
@@ -0,0 +1,20 @@
+<?php
+
+class PageController 
+{
+    public function home(){
+      echo 'Estas en la home';
+    }
+    public function listar(){
+      echo 'Estoy en el metodo listar';
+    }
+    public function modificar(){
+      echo 'Estoy en el metodo  modificar';
+    }
+    public function nuevo(){
+      echo 'Estoy en el metodo nuevo';
+    }
+
+}
+
+?>
